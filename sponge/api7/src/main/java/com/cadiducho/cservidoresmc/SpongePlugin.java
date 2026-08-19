389f2a751a68066d51d24cb30657036ec52a774c
JainaGam3r45 <dubecraft.original@gmail.com>
2026-08-19 18:19:07 -0300

    chore(release): prepare 3.3.0

--- sponge/api7/src/main/java/com/cadiducho/cservidoresmc/SpongePlugin.java
+++ sponge/api7/src/main/java/com/cadiducho/cservidoresmc/SpongePlugin.java
@@ -44,7 +44,7 @@ import java.util.concurrent.Executor;
 @Plugin(id = "cservidoresmc", name = "40ServidoresMC", version = SpongePlugin.PLUGIN_VERSION)
 public class SpongePlugin implements CSPlugin {
 
-    public static final String PLUGIN_VERSION = "3.2.3";
+    public static final String PLUGIN_VERSION = "3.3.0";
     @Inject private Logger logger;
     @Inject private Game game;
 
