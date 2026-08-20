eb2e88538da4ab3a34dd7c6b0ecb601c3cbe76a8
JainaGam3r45 <dubecraft.original@gmail.com>
2026-08-20 13:32:00 -0300

    fix(api): parse mensaje/tipovoto and treat status 0 already rewarded

--- common/src/main/java/com/cadiducho/cservidoresmc/model/VoteResponse.java
+++ common/src/main/java/com/cadiducho/cservidoresmc/model/VoteResponse.java
@@ -6,4 +6,6 @@ import lombok.Data;
 public class VoteResponse {
     private final String web;
     private final VoteStatus status;
+    private final String mensaje;
+    private final String tipovoto;
 }
